82af0a5d55d3386b5578c2c9c683548997bc88a1
JamsAurom <maksimaldokhin84@gmail.com>
2018-03-05 15:54:28 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,270 @@
+var o = [{
+    week0: {
+        day0: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Конструювання та технологія виробництва РЕА лаба',
+                teacher: 'Лисенко О.І.',
+                room: '329-12'
+            },
+            les2: {
+                subject: 'Конструювання та технологія виробництва РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '331а-12'
+            },
+            les3: {
+                subject: 'Психологія',
+                teacher: 'проф. Волянюк Н.Ю',
+                room: '111-7'
+            }
+        },
+        day1: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '414-12'
+            },
+            les2: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '329-12'
+            },
+            les3: {
+                subject: 'Електронні прилади',
+                teacher: 'доц. Вунтесмері',
+                room: '200-12'
+            }
+        },
+        day2: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les2: {
+                subject: 'Основи теорії кіл',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day3: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Вступ до філософії',
+                teacher: 'доц. Зуєв В.М.',
+                room: '321-12'
+            },
+            les2: {
+                subject: 'Вступ до філософії',
+                teacher: 'доц. Зуєв В.М.',
+                room: '106-12'
+            },
+            les3: {
+                subject: 'Функціонально-логічне проектування',
+                teacher: 'Варфоломєєв А.Ю.',
+                room: '339-12'
+            }
+        },
+        day4: {
+            les0: {
+                subject: 'Іноземна мова',
+                teacher: 'Дичка',
+                room: '426-12'
+            },
+            les1: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Белка'
+            },
+            les2: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Лампа'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day5: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        }
+    },
+    week1: {
+        day0: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'Конструювання та технологія виробництва РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '331а-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day1: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '414-12'
+            },
+            les2: {
+                subject: 'Електронні прилади',
+                teacher: 'доц. Вунтесмері',
+                room: '201г-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day2: {
+            les0: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '331-12'
+            },
+            les1: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les2: {
+                subject: 'Основи теорії кіл',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day3: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Функціонально-логічне проект.',
+                teacher: 'ст. в. Варфоломєєв А.Ю.',
+                room: '321-12'
+            },
+            les2: {
+                subject: 'Функціонально-логічне проект.',
+                teacher: 'ст. в. Варфоломєєв А.Ю.',
+                room: '339-12'
+            },
+            les3: {
+                subject: 'Функціонально-логічне проектування',
+                teacher: 'Варфоломєєв А.Ю.',
+                room: '339-12'
+            }
+        },
+        day4: {
+            les0: {
+                subject: 'Іноземна мова',
+                teacher: 'Дичка',
+                room: '426-12'
+            },
+            les1: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Белка'
+            },
+            les2: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Лампа'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day5: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        }
+    }
+}]
---
+++ index.js
@@ -0,0 +1,647 @@
+const o = {
+    week0: {
+        day0: {
+            les0: {
+                subject: "NULL",
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Конструювання та технологія виробництва РЕА лаба',
+                teacher: 'Лисенко О.І.',
+                room: '329-12'
+            },
+            les2: {
+                subject: 'Конструювання та технологія виробництва РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '331а-12'
+            },
+            les3: {
+                subject: 'Психологія',
+                teacher: 'проф. Волянюк Н.Ю',
+                room: '111-7'
+            }
+        },
+        day1: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '414-12'
+            },
+            les2: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '329-12'
+            },
+            les3: {
+                subject: 'Електронні прилади',
+                teacher: 'доц. Вунтесмері',
+                room: '200-12'
+            }
+        },
+        day2: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les2: {
+                subject: 'Основи теорії кіл',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day3: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Вступ до філософії',
+                teacher: 'доц. Зуєв В.М.',
+                room: '321-12'
+            },
+            les2: {
+                subject: 'Вступ до філософії',
+                teacher: 'доц. Зуєв В.М.',
+                room: '106-12'
+            },
+            les3: {
+                subject: 'Функціонально-логічне проектування',
+                teacher: 'Варфоломєєв А.Ю.',
+                room: '339-12'
+            }
+        },
+        day4: {
+            les0: {
+                subject: 'Іноземна мова',
+                teacher: 'Дичка',
+                room: '426-12'
+            },
+            les1: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Белка'
+            },
+            les2: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Лампа'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day5: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        }
+    },
+    week1: {
+        day0: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'Конструювання та технологія виробництва РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '331а-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day1: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Електронна компонентна база РЕА',
+                teacher: 'Лисенко О.І.',
+                room: '414-12'
+            },
+            les2: {
+                subject: 'Електронні прилади',
+                teacher: 'доц. Вунтесмері',
+                room: '201г-12'
+            },
+            les3: {
+                subject: 'Основи теорії кіл',
+                teacher: 'доц. Бондаренко В.М.',
+                room: 'Белка'
+            }
+        },
+        day2: {
+            les0: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '331-12'
+            },
+            les1: {
+                subject: 'Електродинаміка',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les2: {
+                subject: 'Основи теорії кіл',
+                teacher: 'доц. Бондаренко В.М.',
+                room: '312-12'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day3: {
+            les0: {
+                subject: 'ФП',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'Функціонально-логічне проект.',
+                teacher: 'ст. в. Варфоломєєв А.Ю.',
+                room: '321-12'
+            },
+            les2: {
+                subject: 'Функціонально-логічне проект.',
+                teacher: 'ст. в. Варфоломєєв А.Ю.',
+                room: '339-12'
+            },
+            les3: {
+                subject: 'Функціонально-логічне проектування',
+                teacher: 'Варфоломєєв А.Ю.',
+                room: '339-12'
+            }
+        },
+        day4: {
+            les0: {
+                subject: 'Іноземна мова',
+                teacher: 'Дичка',
+                room: '426-12'
+            },
+            les1: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Белка'
+            },
+            les2: {
+                subject: 'Аналогова електроніка',
+                teacher: 'Короткий Є.В.',
+                room: 'Лампа'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        },
+        day5: {
+            les0: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les1: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les2: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            },
+            les3: {
+                subject: 'NULL',
+                teacher: 'NULL',
+                room: 'NULL'
+            }
+        }
+    }
+}
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '566669718:AAGicNjSAeg4cc7W8Ul5IH39aWuSDb79zaY';
+
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+});
+
+const data = new Date();
+
+const keyBoard = {
+    now: 'Что сейчас?',
+    all: 'Рассписание',
+    all_this_week: 'Рассписание эта неделя',
+    tomorrow: 'Что завтра?',
+    day_after_tomorrow: 'Что послезавтра?',
+    another_page_1: 'Вторая страница',
+    another_page_0: 'Первая страница',
+    Laws: 'ДК законы',
+    Files: 'ДК файлы',
+    Democracy: 'ДК демократия',
+    Varf: 'Видео по варфу'
+};
+
+bot.onText(/\/start/, msg => {
+    //console.log('11');
+
+
+    send1keybord0(msg.chat.id, true);
+
+})
+
+bot.on('message', msg => {
+    //console.log(data.getDay());
+    var week_2;
+
+
+    //заглушка
+
+    if(keyBoard.all == msg.text || keyBoard.day_after_tomorrow == msg.text || keyBoard.now == msg.text || keyBoard.tomorrow == msg.text || keyBoard.all_this_week == msg.text)
+    {
+        bot.sendMessage(msg.chat.id, 'Выходной');
+        return;
+    }
+
+
+    switch (msg.text) {
+        case keyBoard.now:
+            week_2 = weekfind(0);
+            switch (data.getDay()) {
+                case 0:
+                    bot.sendMessage(msg.chat.id, 'Выходной');
+                    break;
+                case 1:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day0,0);
+                    else printday(bot,msg,o.week1.day0,0);
+                    break;
+                case 2:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day1,1);
+                    else printday(bot,msg,o.week1.day1,1);
+                    break;
+                case 3:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day2,2);
+                    else printday(bot,msg,o.week1.day2,2);
+                    break;
+                case 4:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day3,3);
+                    else printday(bot,msg,o.week1.day3,3);
+                    break;
+                case 5:
+                    if(week_2 == 0)
+                    printday(bot,msg,o.week0.day4,4);
+                else printday(bot,msg,o.week1.day4,4);
+                    break;
+                case 6:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day5,5);
+                    else printday(bot,msg,o.week1.day5,5);
+                    break;
+            }
+            break;
+        case keyBoard.day_after_tomorrow:
+            week_2 = weekfind(2);
+            var i = data.getDay();
+            if(i + 1 > 6) i = 2;
+            if(i + 2 > 6) i = 1;
+
+            switch (i) {
+                case 0:
+                    i = i + 2;
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week1.day1,0);
+                    else printday(bot,msg,o.week0.day1,0);
+                    break;
+                case 1:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day0,0);
+                    else printday(bot,msg,o.week1.day0,0);
+                    break;
+                case 2:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day1,1);
+                    else printday(bot,msg,o.week1.day1,1);
+                    break;
+                case 3:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day2,2);
+                    else printday(bot,msg,o.week1.day2,2);
+                    break;
+                case 4:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day3,3);
+                    else printday(bot,msg,o.week1.day3,3);
+                    break;
+                case 5:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day4,4);
+                    else printday(bot,msg,o.week1.day4,4);
+                    break;
+                case 6:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day5,5);
+                    else printday(bot,msg,o.week1.day5,5);
+                    break;
+            }
+            break;
+        case keyBoard.tomorrow:
+            week_2 = weekfind(1);
+
+            var i = data.getDay();
+            if(i + 1 > 6) i = 1;
+            console.log(i);
+            switch (i) {
+                case 0:
+                    i = i + 1;
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week1.day0,0);
+                    else printday(bot,msg,o.week0.day0,0);
+                    break;
+                case 1:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day0,0);
+                    else printday(bot,msg,o.week1.day0,0);
+                    break;
+                case 2:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day1,1);
+                    else printday(bot,msg,o.week1.day1,1);
+                    break;
+                case 3:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day2,2);
+                    else printday(bot,msg,o.week1.day2,2);
+                    break;
+                case 4:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day3,3);
+                    else printday(bot,msg,o.week1.day3,3);
+                    break;
+                case 5:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day4,4);
+                    else printday(bot,msg,o.week1.day4,4);
+                    break;
+                case 6:
+                    if(week_2 == 0)
+                        printday(bot,msg,o.week0.day5,5);
+                    else printday(bot,msg,o.week1.day5,5);
+                    break;
+            }
+            break;
+        case keyBoard.all_this_week:
+            if(week_2 == 0) {
+               print_week_all(0, msg, o);
+            }
+            else{
+               print_week_all(1, msg, o);
+            }
+            break;
+        case keyBoard.all:
+            print_week_all(2, msg, o);
+            break;
+        case keyBoard.another_page_1:
+            send1keybord1(msg.chat.id, false);
+            break;
+        case keyBoard.another_page_0:
+            send1keybord0(msg.chat.id, false);
+            break;
+        case keyBoard.Democracy:
+            bot.sendMessage(msg.chat.id, 'https://t.me/joinchat/AAAAAEqGR_gWPkQ1jCraUQ');
+            break;
+        case keyBoard.Files:
+            bot.sendMessage(msg.chat.id, 'https://t.me/joinchat/AAAAAES4YBmgitFMBka_-A');
+            break;
+        case keyBoard.Varf:
+            bot.sendMessage(msg.chat.id, 'https://www.youtube.com/playlist?list=PLybqwkViE-NNowM62SW4g8NTWsKiwF1AM');
+            break;
+        case keyBoard.Laws:
+            bot.sendMessage(msg.chat.id, 'https://t.me/joinchat/AAAAAFIimMwj7R9Xk4eVDw');
+            break;
+    }
+
+
+})
+
+function weekfind(index) {
+
+    var dat = new Date();
+    var i;
+    i = dat.getTime() / 1000; //секунда
+    i = i / 60; // минута
+    i = i / 60;//час
+    i = i / 24;//день
+    i = i - 17567 + index;
+    i = i / 7; //неделя
+    while((Math.round(i) != 0) && (Math.round(i) != 1))
+    {
+        i = i / 2;
+        //console.log(i);
+    }
+    //console.log(i);
+
+    return(Math.floor(i));
+}
+
+function printday(bot, msg, day, number) {
+    var text_day;
+    switch (number){
+        case 0:
+            text_day = '#Понедельник';
+            break;
+        case 1:
+            text_day = '#Вторник';
+            break;
+        case 2:
+            text_day = '#Среда';
+            break;
+        case 3:
+            text_day = '#Четверг';
+            break;
+        case 4:
+            text_day = '#Пятница';
+            break;
+        case 5:
+            text_day = '#Суббота';
+            break;
+
+    }
+    bot.sendMessage(msg.chat.id, text_day + '\n1 пара\n\n' + printles(day.les0) + '\n\n'
+    + '2 пара\n\n' + printles(day.les1) + '\n\n'
+    + '3 пара\n\n' + printles(day.les2) + '\n\n'
+    + '4 пара\n\n' + printles(day.les3));
+    //console.log('111');
+}
+
+function printles(les) {
+    var text_alert, text, text_all;
+    //console.log(les.subject);
+    text_alert = les.subject;
+    if (text_alert != 'NULL') {
+        console.log('0');
+        text = les.teacher;
+        if (text != 'NULL') {
+            //console.log('2');
+            text_all = les.subject + '\n' + les.teacher + '\n' + les.room;
+            return(text_all);
+        }
+        else {
+            text_all = les.subject;
+            return (text_all);
+        }
+    }
+    else
+    return('Нет пары');
+}
+
+function send1keybord0(chatId, sendMes) {
+    const text = sendMes
+        ? 'Приветсвую, Хозяин\nМеня зовут добби\nЯ твой верный слуга\nЯ весь в твоем распоряжении и днем и ночью\nЧто ты хочешь от меня?'
+        : 'Что ты хочешь от меня?';
+
+    bot.sendMessage(chatId, text,
+        {
+            reply_markup:{
+                keyboard:[
+                    [keyBoard.now,keyBoard.tomorrow],
+                    [keyBoard.all,keyBoard.all_this_week],
+                    [keyBoard.day_after_tomorrow,keyBoard.another_page_1]
+                ]
+            }
+        })
+}
+
+function send1keybord1(chatId, sendMes) {
+    bot.sendMessage(chatId, 'Что ты хочешь от меня?',
+        {
+            reply_markup:{
+                keyboard:[
+                    [keyBoard.Files,keyBoard.Democracy],
+                    [keyBoard.Varf,keyBoard.Laws],
+                    [keyBoard.another_page_0]
+                ]
+            }
+        })
+}
+
+function print_week_all(number, msg, o) {
+    var all_rez;
+    if(number == 0 || number == 2)
+    {
+        all_rez = component_day(o.week0.day0, 0) + component_day(o.week0.day1, 1) + component_day(o.week0.day2,2) + component_day(o.week0.day3,3)
+        + component_day(o.week0.day4,4) + component_day(o.week0.day5,5);
+    }
+    if(number == 1 || number == 2)
+    {
+        all_rez = all_rez + component_day(o.week1.day0,0) + component_day(o.week1.day1,1) + component_day(o.week1.day2,2) + component_day(o.week1.day3,3)
+            + component_day(o.week1.day4,4) + component_day(o.week1.day5,5);
+    }
+    //console.log(all_rez);
+    bot.sendMessage(msg.chat.id, all_rez);
+}
+
+function component_day(day, number) {
+    var rez;
+    var text_day;
+    switch (number){
+        case 0:
+            text_day = '#Понедельник';
+            break;
+        case 1:
+            text_day = '#Вторник';
+            break;
+        case 2:
+            text_day = '#Среда';
+            break;
+        case 3:
+            text_day = '#Четверг';
+            break;
+        case 4:
+            text_day = '#Пятница';
+            break;
+        case 5:
+            text_day = '#Суббота';
+            break;
+    }
+    rez = text_day + '\n1 пара\n' + printles1(day.les0) + '\n\n'
+        + '2 пара\n' + printles1(day.les1) + '\n\n'
+        + '3 пара\n' + printles1(day.les2) + '\n\n'
+        + '4 пара\n' + printles1(day.les3) + '\n\n\n';
+    return rez;
+}
+
+function printles1(les) {
+    var text_alert, text, text_all;
+    //console.log(les.subject);
+    text_alert = les.subject;
+    if (text_alert != 'NULL') {
+        console.log('0');
+        text = les.teacher;
+        if (text != 'NULL') {
+            //console.log('2');
+            text_all = les.subject + '\n' + les.room;
+            return(text_all);
+        }
+        else {
+            text_all = les.subject;
+            return (text_all);
+        }
+    }
+    else
+        return('Нет пары');
+}
